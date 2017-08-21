74d978c99d6bfbe70b4a847698ba124eef4de95e
ivntel <ivntel67@gmail.com>
2017-08-20 22:29:49 -0400

    Url put directly into Restclient

--- app/src/main/java/com/ivntel/android/prekhome/Retrofit/NetworkContract.java
+++ app/src/main/java/com/ivntel/android/prekhome/Retrofit/NetworkContract.java
@@ -6,11 +6,6 @@ package com.ivntel.android.prekhome.Retrofit;
 
 public class NetworkContract {
 
-    public final static String HEADER_AUTHONRIZATION ="Basic dGxMY2ZVZjc2SHpXRGpnNXBxZ0R5RkZ3QlpnZEYyb2FiandRQVZFSDo5VkZJbUFNZVhBUlBRZ0NDbXN2NlhlczZYWG5ZZHd6Q0VNc2FRN1M4R2hBcGJqN01pRFViUzlQdXRZSzBHdUIydm1vaHRhRWtrR0VaVU9BaHlPcHJtSWdUQnJyUzF4UmUzVkhDN3JKREg4UjJGZ3A4WVlEMzA1a0pvY25ySUhTdw==";
-    public final static String BASE_URL = "https://www.geniusplaza.com";
-    //public final static String BASE_URL = "http://192.168.1.6:8000";
-    //public final static String BASE_URL = "https://q4.geniusplaza.com";
 
-    public final static String HEADER_CONTENT_TYPE_GSON="application/json";
 
 }
