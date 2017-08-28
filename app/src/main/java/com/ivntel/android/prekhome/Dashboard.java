f38aa9430c1da4e2104dff539d1010bc1148cb9c
ivntel <ivntel67@gmail.com>
2017-08-27 23:16:15 -0400

    Nav drawer with API calls and core functionality intact

--- app/src/main/java/com/ivntel/android/prekhome/Dashboard.java
+++ app/src/main/java/com/ivntel/android/prekhome/Dashboard.java
@@ -1,14 +1,21 @@
 package com.ivntel.android.prekhome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.ivntel.android.prekhome.POJO.Ebook;
 import com.ivntel.android.prekhome.POJO.Example;
@@ -24,12 +31,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static com.ivntel.android.prekhome.R.id.btnSubmit;
 import static com.ivntel.android.prekhome.R.id.spinner1;
-import static com.ivntel.android.prekhome.R.id.spinner2;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Dashboard extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     @Bind(R.id.ebookRecyclerview)
     RecyclerView mEbookRecyclerView;
 
@@ -38,12 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(spinner1)
     Spinner mSpinner1;
-
-    @Bind(spinner2)
-    Spinner mSpinner2;
-
-    @Bind(btnSubmit)
-    Button mButtonSubmit;
 
     private LinearLayoutManager mEbookLayoutManager, mVideoLayoutManager;
     //the adapter that works with the array
@@ -58,12 +57,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
         ButterKnife.bind(this);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         getEbooksAndVideos();
-
-
 
         //sets up Ebook list
         mEbookRecyclerView.setHasFixedSize(false);
@@ -77,9 +83,27 @@ public class MainActivity extends AppCompatActivity {
         mVideoRecyclerView.setLayoutManager(mVideoLayoutManager);
         mVideosAdapter = new VideosAdapter(this, mVideos);
         mVideoRecyclerView.setAdapter(mVideosAdapter);
-
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     public void getEbooksAndVideos() {
         Restclient.getExampleApi().getPreKContents().enqueue(getPreKResults);
@@ -133,8 +157,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    public void submitButton(View v){
-        age = String.valueOf(mSpinner2.getSelectedItem());
+    public void onClickThree(View v){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         language = String.valueOf(mSpinner1.getSelectedItem());
         if(language.equals("Spanish")){
             language = "2";
@@ -142,10 +166,36 @@ public class MainActivity extends AppCompatActivity {
         else{
             language = "1";
         }
+        age = "3";
         getGetPreKNewResults(age, language);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    public void onClickFour(View v){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        language = String.valueOf(mSpinner1.getSelectedItem());
+        if(language.equals("Spanish")){
+            language = "2";
+        }
+        else{
+            language = "1";
+        }
+        age = "4";
+        getGetPreKNewResults(age, language);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    public void onClickFive(View v){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        language = String.valueOf(mSpinner1.getSelectedItem());
+        if(language.equals("Spanish")){
+            language = "2";
+        }
+        else{
+            language = "1";
+        }
+        age = "5";
+        getGetPreKNewResults(age, language);
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
-
-
-
-
